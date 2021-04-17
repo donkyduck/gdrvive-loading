f07216392a0765fa2d771c60ca1829c8c6315b9c
Dongiduck <64454169+donkyduck@users.noreply.github.com>
2021-04-17 12:15:21 +0700

    create test script

---
+++ test_gdrive.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -xe
+
+bash gdrive.sh 0B4y35FiV1wh7QVR6VXJ5dWExSTQ
+rm CRF++-0.58.tar.gz
+bash gdrive.sh https://drive.google.com/open?id=0B4y35FiV1wh7QVR6VXJ5dWExSTQ
+rm CRF++-0.58.tar.gz
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7QVR6VXJ5dWExSTQ/view?usp=sharing
+rm CRF++-0.58.tar.gz
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7QVR6VXJ5dWExSTQ/view
+rm CRF++-0.58.tar.gz
+bash gdrive.sh 0B4y35FiV1wh7SDd1Q1dUQkZQaUU
+rm cabocha-0.69.tar.bz2
+bash gdrive.sh https://drive.google.com/open?id=0B4y35FiV1wh7SDd1Q1dUQkZQaUU
+rm cabocha-0.69.tar.bz2
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7SDd1Q1dUQkZQaUU/view?usp=sharing
+rm cabocha-0.69.tar.bz2
+bash gdrive.sh https://drive.google.com/file/d/0B4y35FiV1wh7SDd1Q1dUQkZQaUU/view
+rm cabocha-0.69.tar.bz2
+bash gdrive.sh https://docs.google.com/file/d/0BwmPMFurnk9Pak5zWEVyOUZESms/edit
+rm Ubuntu-12.04-LTS_x86_64.iso
+#sh gdrive.sh https://drive.google.com/open?id=0B7EVK8r0v71pZjFTYXZWM3FlRnM
+#rm img_align_celeba.zip
+sh gdrive.sh 1TdFtOzKlGQnckOavxlwgJuW4nc9oGU6U
+rm -rf yt/
+sh gdrive.sh https://drive.google.com/drive/folders/0B7EVK8r0v71pOC0wOVZlQnFfaGs
+rm -rf Anno/
+bash gdrive.sh https://drive.google.com/file/d/1g7DgbGAiH87pd-4KOEPR_Uwy657RWtB-/view?usp=sharing
+
+set +xe
